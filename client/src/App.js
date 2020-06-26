204db07ee219c29beae23fb9d14c32fe73e6264e
Daniel <danisan9490@gmail.com>
2020-06-26 18:01:28 +0100

    #1 Create React Basic Home-Page

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,63 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  }
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  }
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="grid-container">
+      <header className="header">
+        <div className="brand">
+          <button onClick={openMenu}>
+            &#9776;
+      </button>
+          <a href="index.html">Brite-U</a>
+        </div>
+        <div className="header-links">
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
+      </header>
+      <aside className="sidebar">
+        <h3>Shopping Categories</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+        <ul>
+          <li>
+            <a href="index.html">Scooters</a>
+          </li>
+          <li>
+            <a href="index.html">Cycles</a>
+          </li>
+        </ul>
+      </aside>
+      <main className="main">
+        <div className="content">
+          <ul className="products">
+            <li>
+              <div className="product">
+                <img className="product-image" src="https://i1.adis.ws/i/washford/843190/Xiaomi-Mi-M365-Electric-Scooter---Black.webp?$sfcc_tile$&w=680" alt="product" />
+                <div className="product-name">
+                  <a href="product.html">scooter</a>
+                </div>
+                <div className="product-brand">Brite-U</div>
+                <div className="product-price">£400</div>
+                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+          </ul>
+
+        </div>
+
+      </main>
+
+      <footer className="footer">
+        All right reserved.
+      </footer>
     </div>
   );
 }
